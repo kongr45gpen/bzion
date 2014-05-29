07f3c8040b85facc17366fc13fccedb722201bad
kongr45gpen <electrovesta@gmail.com>
2014-05-29 15:10:51 +0300

    Add async request method for Pusher

--- src/NotificationAdapter/PusherAdapter.php
+++ src/NotificationAdapter/PusherAdapter.php
@@ -21,7 +21,7 @@ class PusherAdapter extends NotificationAdapter
 
     public function __construct()
     {
-
+        // ????!!!???
     }
 
     /**
@@ -29,7 +29,22 @@ class PusherAdapter extends NotificationAdapter
      */
     public function trigger($channel, $message)
     {
+        $params = json_encode(array(
+            'data' => array ('message' => $message),
+            'name' => 'main_event',
+            'channel' => $channel,
+        ));
 
+        $url  = 'http://api.pusherapp.com/apps/' . PUSHER_APP_ID . '/events';
+        $par  = 'body_md5=' . md5($params);
+        $par .= '&auth_version=1.0';
+        $par .= '&auth_key=' . PUSHER_KEY;
+        $par .= '&auth_timestamp=' . time();
+        $par .= 'auth_signature=' . hash_hmac('sha256', "POST\n$url\n$par", PUSHER_SECRET);
+
+        $time = microtime(true);
+        $this->asyncPost("$url?$par", $params);
+        file_put_contents(__DIR__ . "/top", " - " . (microtime(true)-$time) . "\n", FILE_APPEND);
     }
 
     /**
@@ -41,5 +56,30 @@ class PusherAdapter extends NotificationAdapter
             return false;
 
         return (bool) ENABLE_PUSHER;
+    }
+
+    /**
+     * Perform a POST request without waiting to get a response
+     * @param string $url The URL to perform the request on
+     * @param string $postString The data to provide to the POST request
+     * @return void
+     */
+    private static function asyncPost($url, $postString=null)
+    {
+        $parts=parse_url($url);
+
+        $fp = fsockopen('23.22.231.199',
+            isset($parts['port'])?$parts['port']:80,
+            $errno, $errstr, 30);
+
+        $out = "POST ".$parts['path']." HTTP/1.1\r\n";
+        $out.= "Host: ".$parts['host']."\r\n";
+        $out.= "Content-Type: application/json\r\n";
+        $out.= "Content-Length: ".strlen($postString)."\r\n";
+        $out.= "Connection: Close\r\n\r\n";
+        if ($postString) $out.= $postString;
+
+        fwrite($fp, $out);
+        fclose($fp);
     }
 }
