0c126765584b4fcb540be9991ebc23419c1b96ec
alezakos <electrovesta@gmail.com>
2015-11-18 19:36:06 +0200

    Update 20151117202945_group_rename.php

--- migrations/20151117202945_group_rename.php
+++ migrations/20151117202945_group_rename.php
@@ -7,37 +7,47 @@ class GroupRename extends AbstractMigration
     public function change()
     {
         
-        var_dump($this->fetchAll('SHOW CREATE TABLE `player_groups`;'));
-        var_dump($this->fetchAll('SHOW CREATE TABLE `team_groups`;'));
+        //var_dump($this->fetchAll('SHOW CREATE TABLE `player_groups`;'));
+        //var_dump($this->fetchAll('SHOW CREATE TABLE `team_groups`;'));
         
-        var_dump($this->fetchAll("
-        select
-  TABLE_NAME,COLUMN_NAME,CONSTRAINT_NAME, REFERENCED_TABLE_NAME,REFERENCED_COLUMN_NAME
-from INFORMATION_SCHEMA.KEY_COLUMN_USAGE
-where
-  REFERENCED_TABLE_NAME = 'player_groups' OR REFERENCED_TABLE_NAME='team_groups';
-        "));
+        //var_dump($this->fetchAll("
+     //   select
+ // TABLE_NAME,COLUMN_NAME,CONSTRAINT_NAME, REFERENCED_TABLE_NAME,REFERENCED_COLUMN_NAME
+//from INFORMATION_SCHEMA.KEY_COLUMN_USAGE
+//where
+//  REFERENCED_TABLE_NAME = 'player_groups' OR REFERENCED_TABLE_NAME='team_groups';
+  //      "));
         
-        $this->execute("alter table player_groups drop foreign key player_groups_ibfk_1");
-        $this->execute("alter table player_groups drop foreign key player_groups_ibfk_2");
-        $this->execute("alter table team_groups drop foreign key team_groups_ibfk_1");
-        $this->execute("alter table team_groups drop foreign key team_groups_ibfk_2");
+       // $this->execute("alter table player_groups drop foreign key player_groups_ibfk_1");
+       // $this->execute("alter table player_groups drop foreign key player_groups_ibfk_2");
+        //$this->execute("alter table team_groups drop foreign key team_groups_ibfk_1");
+      //  $this->execute("alter table team_groups drop foreign key team_groups_ibfk_2");
         
-        $this->table('groups')->rename('conversations');
+     //   $this->table('groups')->rename('conversations');
         
         var_dump("hi");
 
-        $this->table('player_groups')
+        $table = $this->dropForeignKeys($this->table('player_groups'))
             ->rename('player_conversations')
             ->renameColumn('group', 'conversation');
 
-        $this->table('team_groups')
+        $this->dropForeignKeys($this->table('team_groups'))
             ->rename('team_conversations')
             ->renameColumn('group', 'conversation');
 
         $messages = $this->table('messages');
+        $this->dropForeignKeys($messages);
         $messages->renameColumn('group_to', 'conversation_to');
         
-
+        die();
+    }
+    
+    private function dropForeignKeys($table) {
+        $keys = $table->getForeignKeys();
+        foreach ($keys as $key) {
+            $table->dropForeignKey($key->getColumns());
+        }
+        
+        return $table;
     }
 }
